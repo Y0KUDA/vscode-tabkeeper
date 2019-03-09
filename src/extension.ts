4fc2b3bdb47a50af10665f0a147de90c2779717a
Y0KUDA <40825115+Y0KUDA@users.noreply.github.com>
2019-03-10 01:21:49 +0900

    add db

--- src/extension.ts
+++ src/extension.ts
@@ -2,24 +2,52 @@ import * as vscode from "vscode";
 import { workspace, commands, ExtensionContext, window } from "vscode";
 import * as fs from "fs";
 export function activate(context: ExtensionContext) {
+  let db = Array();
+
+  fs.readFile(workspace.rootPath + "/.tabkeeper.json", "utf8", (err, json) => {
+    db=JSON.parse(json);
+  });
+
   context.subscriptions.push(
     commands.registerCommand("extension.saveTabs", () => {
-      fs.writeFile(workspace.rootPath + "/.tabkeeper", "", err => {}); // delete list in file
-      workspace.textDocuments.forEach(doc => {
+      let tabs = Object();
+      window.showInputBox({ prompt: "name?" }).then(name => {
+        if(name===''){
+          name='default';
+        }
+        tabs = {
+          name: name,
+          paths: []
+        };
+        workspace.textDocuments.forEach(doc => {
+          tabs["paths"].push(doc.uri.fsPath);
+        });
+        
+        // remove duplicates
+        db.forEach((obj,i)=>{
+          if(tabs.name === obj.name){
+            db.splice(i,1);
+          }
+        });
+
+        db.push(tabs);
         fs.writeFile(
-          workspace.rootPath + "/.tabkeeper",
-          doc.uri.fsPath + "\n",
-          { flag: "a" },
-          err => {}
+          workspace.rootPath + "/.tabkeeper.json",
+          JSON.stringify(db, null , "\t"),
+          err => { }
         );
       });
     }),
     vscode.commands.registerCommand("extension.popTabs", () => {
-      fs.readFile(workspace.rootPath + "/.tabkeeper", "utf8", (err, paths) => {
-        paths.split("\n").forEach(path => {
-          workspace
-            .openTextDocument(path)
-            .then(doc => window.showTextDocument(doc, { preview: false }));
+      window.showQuickPick(db.map(obj => obj.name),{  }).then(name => {
+        db.forEach((obj)=>{
+          if(name === obj.name){
+            obj.paths.forEach((path:string)=>{
+              workspace
+              .openTextDocument(path)
+              .then(doc => window.showTextDocument(doc, { preview: false }));
+            });
+          }
         });
       });
     })
